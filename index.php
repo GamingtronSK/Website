cf040df27a6528c2131ffb405f71ccb666c5da1a
GamingtronSK <106032900+GamingtronSK@users.noreply.github.com>
2025-09-23 11:14:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include('./config.php');
+$nadpis = WEB_AUTHOR;
+$popis = 'Daco';
+?>
+</head>
+<title><?php echo(WEB_AUTHOR); ?></title>
+<link rel="stylesheet" type="text/css" href="./assets/css/root.css">
+</head>
+
+</body>
+<h1><?php echo(WEB_AUTHOR); ?></h1>
+<p><?php echo($popis); ?></p>
+
+</body>
+</html>
